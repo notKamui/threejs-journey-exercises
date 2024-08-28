67b696002ded8bc00b7cef8c4b4200a3ff76bb6f
notKamui <teillard.jimmy@outlook.fr>
2024-08-29 01:36:37 +0200

    L07

---
+++ L07/src/index.ts
@@ -0,0 +1,87 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl') as HTMLCanvasElement
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Object
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xFF0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 3
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({ canvas })
+
+function resizeCanvas() {
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+}
+
+resizeCanvas()
+window.addEventListener('resize', resizeCanvas)
+
+window.addEventListener('dblclick', () => {
+  const fullscreenElement: Element | null = document.fullscreenElement ?? (document as any).webkitFullscreenElement
+  if (!fullscreenElement) {
+    canvas.requestFullscreen?.();
+    (canvas as any).webkitRequestFullscreen?.()
+  } else {
+    document.exitFullscreen?.();
+    (document as any).webkitExitFullscreen?.()
+  }
+})
+
+/**
+ * Animate
+ */
+// const clock = new THREE.Clock()
+
+function tick() {
+  // const elapsedTime = clock.getElapsedTime()
+
+  // Update controls
+  controls.update()
+
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
+}
+
+tick()
